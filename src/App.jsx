5d61f16566256df01ca5ad623b89abba3d1c67bf
unknown <waibaniraj208@gmail.com>
2024-03-15 15:53:13 +0545

    Creating Ecommerce

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header className="section-navbar">
+        <section>
+          <div className="head container">
+            <div className="head_txt">
+              <p>Free shipping, 30-day return guarantee</p>
+            </div>
+
+            <div className="sing_in_up">
+              <a href="#">SIGN IN</a>
+              <a href="#">SIGN UP</a>
+            </div>
+          </div>
+        </section>
+      </header>
+    </>
+  );
+}
+
+export default App;
