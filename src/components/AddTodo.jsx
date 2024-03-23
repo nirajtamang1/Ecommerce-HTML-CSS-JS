b10b69c7ca6c60043b435e9c9832ba017b7b3874
unknown <waibaniraj208@gmail.com>
2024-03-23 16:50:13 +0545

    Creating Todo list

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../features/todo/todoSlice";
+
+function AddTodo() {
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+  const addTodoHandler = (e) => {
+    e.preventDefault();
+    dispatch(addTodo(input));
+    setInput("");
+  };
+  return (
+    <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
+      <input
+        type="text"
+        placeholder="Enter a  todo..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button type="submit">Add to DO</button>
+    </form>
+  );
+}
+
+export default AddTodo;
